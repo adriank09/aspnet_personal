b43e6e01ce02614a9f1e2e5058e0a1488b5d64fa
adriank09 <adriank96@outlook.com>
2017-09-17 21:28:11 +0800

    Add project files.

---
+++ AdrianKhor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AdrianKhor.MvcApplication" Language="C#" %>
---
+++ AdrianKhor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdrianKhor.Startup))]
+namespace AdrianKhor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
